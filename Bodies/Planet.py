342ed60212682303e2deaba05fd4f72486397518
Matt Kirby <31753757+mkirby1995@users.noreply.github.com>
2019-05-30 15:03:46 -0500

    Add more attributes

--- Bodies/Planet.py
+++ Bodies/Planet.py
@@ -3,7 +3,8 @@ class Planet():
     This object represents a planet
     _________________________________________________
     Attributes:
-    name, mass, aphelion, perihelion, semi_major_axis
+    name, mass, aphelion, perihelion, semi_major_axis,
+    eccentricity, orbital_period, synodic_peroid
     _________________________________________________
     """
     def __init__(self,
@@ -11,10 +12,18 @@ class Planet():
                  mass=None,
                  aphelion = None,
                  perihelion = None,
-                 semi_major_axis = None):
+                 semi_major_axis = None,
+                 eccentricity = None,
+                 orbital_period = None,
+                 synodic_peroid = None
+                ):
+        
         self.name = name
         self.mass = mass
         self.aphelion = aphelion
         self.perihelion = perihelion
         self.semi_major_axis = semi_major_axis
+        self.eccentricity = eccentricity
+        self.orbital_period = orbital_period
+        self.synodic_peroid = synodic_peroid
         
