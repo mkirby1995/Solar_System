dcffcc6a085644b7ecdaadf07f054cfd467e431c
Matt Kirby <31753757+mkirby1995@users.noreply.github.com>
2019-05-30 14:47:51 -0500

    Add docstring and more attributes

--- Bodies/Planet.py
+++ Bodies/Planet.py
@@ -1,7 +1,20 @@
 class Planet():
     """
     This object represents a planet
+    _________________________________________________
+    Attributes:
+    name, mass, aphelion, perihelion, semi_major_axis
+    _________________________________________________
     """
-    def __init__(self, name=None, mass = None):
+    def __init__(self,
+                 name=None,
+                 mass=None,
+                 aphelion = None,
+                 perihelion = None,
+                 semi_major_axis = None):
         self.name = name
         self.mass = mass
+        self.aphelion = aphelion
+        self.perihelion = perihelion
+        self.semi_major_axis = semi_major_axis
+        
